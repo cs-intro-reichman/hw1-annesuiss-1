ed3bb51964046e52495702c5a7c89a352b06972f
annesuiss <annesuisa@gmail.com>
2025-10-29 16:59:16 +0200

    Update Bill3.java
    Hw1 Q1 done.

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,16 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		int totalBill = Integer.parseInt(args[3]);
+
+		// compute the payment per each person
+		double billPerPerson = Math.ceil(totalBill / 3.0);
+
+		//prints the requested line:
+		System.out.print ("Dear " + name3 + ", " + name2 + " and " + name1 + ": pay" +
+						  billPerPerson + " shekels each.");
+	
 	}
 }
